722e37de5927f99c840a0e4f402445b9e56ba7a0
gracebir <grace.devolop@gmail.com>
2023-05-21 16:34:10 +0200

    footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
+import { footeritems } from "../data"
+
 function Footer() {
   return (
-    <footer>
-      
+    <footer className="bg-[#f2f2f2] absolute bottom-0 left-0 right-0 text-[#70757a] text-[.9rem]">
+      <div className="px-4 py-3 md:px-8">
+        <span>Rwanda</span>
+      </div>
+      <hr />
+      <div className="flex flex-col items-center md:justify-between md:flex-row px-4 md:px-8">
+        <div className="flex flex-row py-4 gap-3">
+          {footeritems.slice(0, 4).map((item, i)=> (
+            <a className="text-gray-600 hover:underline" href="#" key={i}>{item}</a>
+          ))}
+        </div>
+        <div className="flex flex-row py-4 gap-3">
+          {footeritems.slice(4, footeritems.length).map((item, i)=> (
+            <a className="text-gray-600 hover:underline" href="#" key={i}>{item}</a>
+          ))}
+        </div>
+      </div>
     </footer>
   )
 }
