5f072c7b1da0c40d6e1d3b1570d5fcd513471c39
gracebir <grace.devolop@gmail.com>
2023-05-21 16:11:17 +0200

    component call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Search from "./components/Search"
+
 function App() {
   return (
-    <div>
-      
+    <div className="bg-white h-screen font-global-font">
+      <Navbar/>
+      <Search/>
+      <Footer/>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        'search-shadow':"0 1px 6px rgba(32,33,36,.28)",
+        "btn-search": "0 1px 1px rgba(0,0,0,.1)"
+      },
+      colors: {
+        "btn-bg": "#f8f9fa"
+      },
+      fontFamily: {
+        "global-font": ["arial", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
